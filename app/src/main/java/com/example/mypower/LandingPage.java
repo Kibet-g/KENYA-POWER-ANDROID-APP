418dfc86cde26e48d024c861323bad7fa285d1ef
KIBET KORIR <93614316+Kibet-g@users.noreply.github.com>
2023-07-26 13:41:36 +0300

    KENYA POWER MAIN APP

--- app/src/main/java/com/example/mypower/LandingPage.java
+++ app/src/main/java/com/example/mypower/LandingPage.java
@@ -13,24 +13,95 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class LandingPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class LandingPage extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);// Use the combined layout
+        setContentView(R.layout.activity_landing_page);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
+        // Find the navigation drawer menu items by their IDs
+        MenuItem homeMenuItem = navigationView.getMenu().findItem(R.id.home);
+        MenuItem newApplMenuItem = navigationView.getMenu().findItem(R.id.new_appl);
+        MenuItem feedbackMenuItem = navigationView.getMenu().findItem(R.id.feed_back);
+        MenuItem failureMenuItem = navigationView.getMenu().findItem(R.id.power_failure);
+        MenuItem billtokenMenuItem= navigationView.getMenu().findItem(R.id.bill_token);
+        MenuItem aboutMenuItem = navigationView.getMenu().findItem(R.id.about_a);
+        MenuItem faqsMenuItem = navigationView.getMenu().findItem(R.id.faqs);
+
+
+        // Set OnClickListener for each menu item
+        homeMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        newApplMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NewApplicationsFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        feedbackMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedbackFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        failureMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportFailureFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+        billtokenMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MybillTokenFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        aboutMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragmentFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        faqsMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FAQSFragment()).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -38,40 +109,6 @@ public class LandingPage extends AppCompatActivity implements NavigationView.OnN
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.home);
         }
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                break;
-
-
-            case R.id.new_appl:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NewApplicationsFragment()).commit();
-                break;
-
-            case R.id.feed_back:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedbackFragment()).commit();
-                break;
-
-            case R.id.power_failure:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportFailureFragment()).commit();
-                break;
-
-            case R.id.about_a:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragmentFragment()).commit();
-                break;
-
-            case R.id.faqs:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FAQSFragment()).commit();
-                break;
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
     }
 
     @Override
@@ -83,4 +120,3 @@ public class LandingPage extends AppCompatActivity implements NavigationView.OnN
         }
     }
 }
-
