19b465ae83efbf874946715d9531497538e73cc3
KIBET KORIR <93614316+Kibet-g@users.noreply.github.com>
2023-07-26 09:21:47 +0300

    KENYA POWER MAIN APP

--- app/src/main/java/com/example/mypower/LandingPage.java
+++ app/src/main/java/com/example/mypower/LandingPage.java
@@ -1,28 +1,86 @@
 package com.example.mypower;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
 
-public class LandingPage extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-    Button sidebar;
+import com.google.android.material.navigation.NavigationView;
+
+public class LandingPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_landing_page);// Use the combined layout
 
-        sidebar = findViewById(R.id.sidebar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        sidebar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, sidebarMenuActivity.class);
-                startActivity(intent);
-            }
-        });
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
+                R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+
+
+            case R.id.new_appl:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new NewApplicationsFragment()).commit();
+                break;
+
+            case R.id.feed_back:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FeedbackFragment()).commit();
+                break;
+
+            case R.id.power_failure:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ReportFailureFragment()).commit();
+                break;
+
+            case R.id.about_a:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AboutFragmentFragment()).commit();
+                break;
+
+            case R.id.faqs:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FAQSFragment()).commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
